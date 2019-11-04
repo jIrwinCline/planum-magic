45fadd1ab2fcae4da0462f175bd1d9e2b0fae693
jirwincline <jakobinwins@gmail.com>
2019-11-03 22:15:29 -0800

    session token is returned after login

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,38 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+const app = require('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+var firebaseConfig = {
+  apiKey: "AIzaSyDMLR-QTpzz8WDMrAS4A1j9UJj8Ea_bXfM",
+  authDomain: "planum-magic.firebaseapp.com",
+  databaseURL: "https://planum-magic.firebaseio.com",
+  projectId: "planum-magic",
+  storageBucket: "planum-magic.appspot.com",
+  messagingSenderId: "115892163015",
+  appId: "1:115892163015:web:d44104c478bb31447409e2",
+  measurementId: "G-XY1RV01RWB"
+};
+
+const firebase = require('firebase')
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
+
+//helper functions
+
+const isEmpty = string => {
+  if (string.trim() === "") return true;
+  else return false;
+};
 
 // GET ALL posts route
 
 app.get('/posts', (req,res) => {
-    admin
-      .firestore()
+    db
       .collection("posts")
+      .orderBy('createdAt', 'desc')
       .get()
       .then(data => {
         let posts = [];
@@ -48,8 +69,7 @@ app.post('/post', (req, res) => {
     highEnd: req.body.highEnd,
     createdAt: new Date().toISOString()
     };
-  admin
-    .firestore()
+  db
     .collection("posts")
     .add(newPost)
     .then(doc => {
@@ -60,5 +80,33 @@ app.post('/post', (req, res) => {
       console.error(err);
     });
 });
+
+// Login Route
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = "Must not be empty";
+    if(isEmpty(user.password)) errors.password = "Must not be empty";
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+        return data.user.getIdToken();
+    })
+    .then(token => {
+        return res.json({token});
+    })
+    .catch(err => {
+        console.error(err);
+        return res.stattus(500).json({error: err.code})
+    })
+})
 
 exports.api = functions.https.onRequest(app)
