60dbe0381f91ffbe61cb50ec9d23aafd3aeba887
jirwincline <jakobinwins@gmail.com>
2019-11-03 23:08:38 -0800

    create auth as middleware

--- functions/index.js
+++ functions/index.js
@@ -55,9 +55,41 @@ app.get('/posts', (req,res) => {
       .catch(err => console.error(err));
 });
 
+
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('no token found')
+        return res.status(403).json({ error: 'Unauthorized' });
+    }
+
+    admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+        // req.user = decodedToken;
+        // return db.collection('users')
+        // .where('userId', '==', req.user.uid)
+        // .limit(1)
+        // .get()
+        if(!decodedToken){
+            console.error("Error while verifying the token", err);
+            return res.status(403).json(err);
+        }
+        return next();
+    })
+    // .then(data => {
+    //     req.user.handle = data.docs[0].data().handle;
+    //     return next();
+    // })
+    .catch(err => {
+        console.error('Error while verifying the token', err);
+        return res.status(403).json(err)
+    })
+}
 // CREATE Post route
 
-app.post('/post', (req, res) => {
+app.post('/post', FBAuth, (req, res) => {
     const newPost = {
     name: req.body.name,
     images: req.body.images,
