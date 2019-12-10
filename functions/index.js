84494b45b5520602db30b636f51b4fe2542d9699
jirwincline <jakobinwins@gmail.com>
2019-12-09 21:54:05 -0800

    static

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,14 @@ app.use(cors());
 const { getAllPosts, createOnePost, getThePost, deletePost, uploadImage } = require('./handlers/posts');
 const { login } = require('./handlers/users');
 
+
+// app.get("/", function(req, res) {
+//   res.render(path.join(__dirname + "/build/index.html"));
+// });
+// app.get("/login", function(req, res) {
+//   res.render(path.join(__dirname + "/build/index.html"));
+// });
+
 // Posts Routes
 
 app.get('/posts', getAllPosts);
@@ -18,11 +26,9 @@ app.get('/post/:postId', getThePost);
 app.post("/post", FBAuth, createOnePost);
 app.delete('/post/:postId', FBAuth, deletePost);
 app.post('/post/:postId/image', FBAuth, uploadImage);
-
 //TODO update post
 
 // Login Route
-
 app.post('/login', login)
 
 exports.api = functions.https.onRequest(app)
