f265ee4d620f1d4406621d56ddf27cd29f3c35ad
Jacob Irwin <47622929+jIrwinCline@users.noreply.github.com>
2019-12-11 12:27:43 -0800

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: "AIzaSyDMLR-QTpzz8WDMrAS4A1j9UJj8Ea_bXfM",
-  authDomain: "planum-magic.firebaseapp.com",
-  databaseURL: "https://planum-magic.firebaseio.com",
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxx",
+  databaseURL: "https://xxxxxxxxxxxxxxxxxxxx",
   projectId: "planum-magic",
-  storageBucket: "planum-magic.appspot.com",
-  messagingSenderId: "115892163015",
-  appId: "1:115892163015:web:d44104c478bb31447409e2",
-  measurementId: "G-XY1RV01RWB"
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxxxxxxxxxx"
 };
